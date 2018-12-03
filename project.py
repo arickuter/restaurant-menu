452949478e7ef120d974f51f195074479b5e1755
Aric <arickuter99@gmail.com>
2018-12-03 13:05:14 +0200

    Route when certain url entered

--- project.py
+++ project.py
@@ -31,18 +31,22 @@ def restaurantMenu(restaurant_id):
 # Task 1: Create route for newMenuItem function here
 
 
+@app.route('/restaurant/<int:restaurant_id>/new/')
 def newMenuItem(restaurant_id):
+
     return "page to create a new menu item. Task 1 complete!"
 
 # Task 2: Create route for editMenuItem function here
 
 
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit/')
 def editMenuItem(restaurant_id, menu_id):
     return "page to edit a menu item. Task 2 complete!"
 
 # Task 3: Create a route for deleteMenuItem function here
 
 
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete/')
 def deleteMenuItem(restaurant_id, menu_id):
     return "page to delete a menu item. Task 3 complete!"
 
