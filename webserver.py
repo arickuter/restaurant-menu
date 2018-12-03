f79cec95a77258a8e0dadf8be46c177ff3aafaa0
Aric <arickuter99@gmail.com>
2018-12-03 09:40:12 +0200

    Edit restaurant name page Post request for editing name

--- webserver.py
+++ webserver.py
@@ -58,7 +58,7 @@ class webServerHandler(BaseHTTPRequestHandler):
                 output += "<a href=/restaurants/new >Create new restaurant</a></br></br>"
                 for restaurant in restaurants:
                     output += restaurant.name
-                    output += "</br><a href=#>Edit</a>"
+                    output += "</br><a href ='/restaurants/%s/edit' >Edit </a> " % restaurant.id
                     output += "</br><a href=#>Delete</a>"
                     output += "</br></br></br>"
                 output += "</body></html>"
@@ -81,11 +81,52 @@ class webServerHandler(BaseHTTPRequestHandler):
                 self.wfile.write(output)
                 return
 
+            # Editing restaurant name page
+            if self.path.endswith("/edit"):
+                restaurantIDPath = self.path.split("/")[2]
+                myRestaurantQuery = session.query(Restaurant).filter_by(
+                    id=restaurantIDPath).one()
+                if myRestaurantQuery:
+                    self.send_response(200)
+                    self.send_header('Content-type', 'text/html')
+                    self.end_headers()
+                    output = "<html><body>"
+                    output += "<h1>"
+                    output += myRestaurantQuery.name
+                    output += "</h1>"
+                    output += "<form method='POST' enctype='multipart/form-data' action = '/restaurants/%s/edit' >" % restaurantIDPath
+                    output += "<input name = 'newRestaurantName' type='text' placeholder = '%s' >" % myRestaurantQuery.name
+                    output += "<input type = 'submit' value = 'Rename'>"
+                    output += "</form>"
+                    output += "</body></html>"
+
+                    self.wfile.write(output)
+
         except IOError:
             self.send_error(404, 'File Not Found: %s' % self.path)
 
     def do_POST(self):
         try:
+            # Post request to edit restaurant name
+            if self.path.endswith("/edit"):
+                ctype, pdict = cgi.parse_header(
+                    self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    messagecontent = fields.get('newRestaurantName')
+                    restaurantIDPath = self.path.split("/")[2]
+
+                    myRestaurantQuery = session.query(Restaurant).filter_by(
+                        id=restaurantIDPath).one()
+                    if myRestaurantQuery != []:
+                        myRestaurantQuery.name = messagecontent[0]
+                        session.add(myRestaurantQuery)
+                        session.commit()
+                        self.send_response(301)
+                        self.send_header('Content-type', 'text/html')
+                        self.send_header('Location', '/restaurants')
+                        self.end_headers()
+
             # Post request for new restaurant
             if self.path.endswith("/restaurants/new"):
                 ctype, pdict = cgi.parse_header(
