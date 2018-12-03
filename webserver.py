c755cda97477270f756b8ea2e29030a4442dd20e
Aric <arickuter99@gmail.com>
2018-12-03 08:46:04 +0200

    Connect to db and display all restaurants

--- webserver.py
+++ webserver.py
@@ -1,6 +1,18 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
 
+# import CRUD Operations from Lesson 1
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+# Create session and connect to DB
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 
 class webServerHandler(BaseHTTPRequestHandler):
 
@@ -31,6 +43,20 @@ class webServerHandler(BaseHTTPRequestHandler):
                 self.wfile.write(output)
                 print output
                 return
+
+            if self.path.endswith("/restaurants"):
+                restaurants = session.query(Restaurant).all()
+                output = ""
+                self.send_response(200)
+                self.send_header('Content-type',	'text/html')
+                self.end_headers()
+
+                output += "<html><body>"
+                for restaurant in restaurants:
+                    output += restaurant.name
+                    output += "</br></br></br>"
+                output += "</body></html>"
+                self.wfile.write(output)
 
         except IOError:
             self.send_error(404, 'File Not Found: %s' % self.path)
