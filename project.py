9904c1621929c540a6eba6551983e3df2f3ef269
Aric <arickuter99@gmail.com>
2018-12-03 10:52:09 +0200

    Retrieve menu items for first restaurant from db

--- project.py
+++ project.py
@@ -1,11 +1,28 @@
 from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
 app = Flask(__name__)
 
 
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
 @app.route('/')
-@app.route('/hello')
+@app.route('/restaurants')
 def HelloWorld():
-    return "Hello World"
+    restaurant = session.query(Restaurant).first()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+    output = ''
+    for i in items:
+        output += i.name
+        output += '</br>'
+    return output
+
 
 if __name__ == '__main__':
     app.debug = True
